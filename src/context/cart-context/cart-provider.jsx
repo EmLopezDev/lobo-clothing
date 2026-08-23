650c762b6c38118712a978bf9b266c539b1e5935
Emmanuel <emlopezdev@gmail.com>
2026-08-23 14:52:10 -0400

    feature: updated CartContext to use useReducer instead of state

--- src/context/cart-context/cart-provider.jsx
+++ src/context/cart-context/cart-provider.jsx
@@ -1,5 +1,11 @@
-import { useState, useCallback, useMemo } from "react";
+import { useCallback, useMemo, useReducer } from "react";
 import { CartContext } from "./cart-context";
+import { CART_ACTION_TYPES } from "./cart-actions";
+
+const INITIAL_STATE = {
+    isCartOpen: false,
+    cartItems: [],
+};
 
 const addOrUpdateCartItem = (cartItems, productToAdd) => {
     const existingCartItem = cartItems.find((item) => item.id === productToAdd.id);
@@ -32,9 +38,42 @@ const clearCartItem = (cartItems, productToClear) => {
     return cartItems.filter((cartItem) => cartItem.id !== productToClear.id);
 };
 
+const cartReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case CART_ACTION_TYPES.TOGGLE_CART:
+            return {
+                ...state,
+                isCartOpen: !state.isCartOpen,
+            };
+        case CART_ACTION_TYPES.SET_CART_ITEMS:
+            return {
+                ...state,
+                cartItems: payload,
+            };
+        case CART_ACTION_TYPES.ADD_ITEM_TO_CART:
+            return {
+                ...state,
+                cartItems: addOrUpdateCartItem(state.cartItems, payload),
+            };
+        case CART_ACTION_TYPES.REMOVE_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: removeCartItem(state.cartItems, payload),
+            };
+        case CART_ACTION_TYPES.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: clearCartItem(state.cartItems, payload),
+            };
+        default:
+            throw new Error(`Unhandled type ${type} in cartReducer`);
+    }
+};
+
 export const CartProvider = ({ children }) => {
-    const [isCartOpen, setIsCartOpen] = useState(false);
-    const [cartItems, setCartItems] = useState([]);
+    const [{ isCartOpen, cartItems }, dispatch] = useReducer(cartReducer, INITIAL_STATE);
 
     const cartCount = useMemo(
         () =>
@@ -52,26 +91,21 @@ export const CartProvider = ({ children }) => {
         [cartItems],
     );
 
-    const addItemToCart = useCallback(
-        (productToAdd) => {
-            setCartItems(addOrUpdateCartItem(cartItems, productToAdd));
-        },
-        [cartItems],
-    );
+    const setIsCartOpen = useCallback(() => {
+        dispatch({ type: CART_ACTION_TYPES.TOGGLE_CART });
+    }, []);
 
-    const decrementItemInCart = useCallback(
-        (product) => {
-            setCartItems(removeCartItem(cartItems, product));
-        },
-        [cartItems],
-    );
+    const addItemToCart = useCallback((productToAdd) => {
+        dispatch({ type: CART_ACTION_TYPES.ADD_ITEM_TO_CART, payload: productToAdd });
+    }, []);
 
-    const clearItemFromCart = useCallback(
-        (product) => {
-            setCartItems(clearCartItem(cartItems, product));
-        },
-        [cartItems],
-    );
+    const decrementItemInCart = useCallback((productToRemove) => {
+        dispatch({ type: CART_ACTION_TYPES.REMOVE_ITEM_FROM_CART, payload: productToRemove });
+    }, []);
+
+    const clearItemFromCart = useCallback((productToClear) => {
+        dispatch({ type: CART_ACTION_TYPES.CLEAR_ITEM_FROM_CART, payload: productToClear });
+    }, []);
 
     const value = useMemo(
         () => ({
