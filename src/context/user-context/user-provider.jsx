644d0cbcba12fe5dd30e2b87a42e3ae8f84e8a74
Emmanuel <emlopezdev@gmail.com>
2026-08-23 12:04:57 -0400

    feature: updated UserContext to use useReducer instead of useState

--- src/context/user-context/user-provider.jsx
+++ src/context/user-context/user-provider.jsx
@@ -1,12 +1,35 @@
-import { useState, useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import { UserContext } from "./user-context";
+import { USER_ACTION_TYPES } from "./user-actions";
 import {
     onAuthStateChangedListener,
     createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase";
 
+const INITIAL_STATE = {
+    currentUser: null,
+};
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload,
+            };
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+};
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+    const setCurrentUser = (user) => {
+        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    };
 
     useEffect(() => {
         const unsubscribe = onAuthStateChangedListener((user) => {
