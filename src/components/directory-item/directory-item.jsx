2a82ac3dd7efec7db8a6bf0a15c2dd3bcb534dba
Emmanuel <emlopezdev@gmail.com>
2026-08-22 20:05:03 -0400

    feature: converted DirectoryItem to be a button to add navigation onClick

--- src/components/directory-item/directory-item.jsx
+++ src/components/directory-item/directory-item.jsx
@@ -1,9 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import "./directory-item.scss";
 
 const DirectoryItem = ({ category }) => {
     const { title, imageUrl } = category;
+    const navigate = useNavigate();
+
+    const navigateHandler = () => {
+        navigate(`shop/${title.toLowerCase()}`);
+    };
+
     return (
-        <div className="directory-item-container">
+        <button
+            className="directory-item-container"
+            onClick={navigateHandler}
+        >
             <div
                 className="background-image"
                 style={{
@@ -14,7 +24,7 @@ const DirectoryItem = ({ category }) => {
                 <h2>{title}</h2>
                 <p>Shop Now</p>
             </div>
-        </div>
+        </button>
     );
 };
 
