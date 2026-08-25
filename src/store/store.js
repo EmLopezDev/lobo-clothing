2c6eaa4b157243705cea46a4b6bbc68fe99c3654
Emmanuel <emlopezdev@gmail.com>
2026-08-25 16:46:21 -0400

    feature: incorporated redix-persist and redux-devtools for chrome

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,31 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import { rootReducer } from "./root-reducer";
+import { persistReducer, persistStore } from "redux-persist";
 
 const logger = createLogger();
 
-const middleWares = [logger];
+const customWebStorage = {
+    getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+    setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
+    removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
+};
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const persistConfig = {
+    key: "root",
+    storage: customWebStorage,
+    blacklist: ["user"],
+};
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [!import.meta.env.PROD && logger].filter(Boolean);
+
+const composeEnhancer =
+    (!import.meta.env.PROD && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
